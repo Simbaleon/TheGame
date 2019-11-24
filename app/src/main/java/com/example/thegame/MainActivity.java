8e5536462ecb6add006ff6f066b8af6757abebb4
Polina Belikova <simbaleon1980@gmail.com>
2019-11-24 18:40:27 +0300

    Day_3 (Link Clicks without new levels)

--- app/src/main/java/com/example/thegame/MainActivity.java
+++ app/src/main/java/com/example/thegame/MainActivity.java
@@ -13,7 +13,10 @@ public class MainActivity extends AppCompatActivity {
     public static int Coins;
     public static int level = 1;
     public String Name = "";
-    private int ClickB2, ClickB3;
+    private int ClickB2, ClickB3, Click;
+    private int ForClickB2, ForClickB3;
+    private boolean ClB1, ClB2, ClB3;
+    private String ForClickB2String, ForClickB3String;
 
     public Button btn1, btn2, btn3;
 
@@ -22,13 +25,13 @@ public class MainActivity extends AppCompatActivity {
         Coins = 90;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn1 = (Button)findViewById(R.id.button1);
+        btn1 = (Button) findViewById(R.id.button1);
         btn1.setText("Заработок");
         btn1.setBackgroundColor(Color.GREEN);
-        btn2 = (Button)findViewById(R.id.button2);
+        btn2 = (Button) findViewById(R.id.button2);
         btn2.setText("Откроется на уровне 2");
         btn2.setBackgroundColor(Color.GRAY);
-        btn3 = (Button)findViewById(R.id.button3);
+        btn3 = (Button) findViewById(R.id.button3);
         btn3.setText("Откроется на уровне 5");
         btn3.setBackgroundColor(Color.GRAY);
         LevelsView(level);
@@ -42,19 +45,27 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        ClB1 = true;
+                        ClB2 = false;
+                        ClB3 = false;
+                        Click ++;
                         Button1 a = new Button1();
                         Levels L = new Levels();
                         level = L.AddLevel(Coins);
                         LevelsView(level);
+
 
                         Coins = a.Task1(Coins);
                         level = L.AddLevel(Coins);
                         LevelsView(level);
                         CoinsView(Coins);
 
+                        if (ClickB2 < 15){ ClickB2 ++;}
+                        if (ClickB3 < 30){ ClickB3 ++;}
                         ForButton1(a.Task3(level));
                         ForButton2(level);
                         ForButton3(level);
+                        Click ++;
 
                     }
                 });
@@ -62,19 +73,25 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        Click ++;
+                        ClB1 = false;
+                        ClB2 = true;
+                        ClB3 = false;
                         Button2 b = new Button2();
                         Levels L = new Levels();
                         level = L.AddLevel(Coins);
                         LevelsView(level);
                         CoinsView(Coins);
-                        if (level < 2){
+                        if (level < 2) {
 
-                        }else if (Coins < b.Task2(level)){
+                        } else if(ClickB2 < 15){
+                            ForButton2(level);
+                        } else if (Coins < b.Task2(level)) {
                             Toast.makeText(
                                     MainActivity.this, "Недостаточно средств",
                                     Toast.LENGTH_LONG
                             ).show();
-                        }else {
+                        } else {
 
                             Coins = Coins - b.Task2(level) + b.Task1(Coins);
                             level = L.AddLevel(Coins);
@@ -82,6 +99,8 @@ public class MainActivity extends AppCompatActivity {
                             CoinsView(Coins);
                             ForButton2(level);
                             ForButton3(level);
+                            if (ClickB3 < 30){ ClickB3 ++;}
+
                         }
                     }
                 });
@@ -89,52 +108,94 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        Click++;
+                        ClB1 = false;
+                        ClB2 = false;
+                        ClB3 = true;
                         Button3 c = new Button3();
                         Levels L = new Levels();
                         level = L.AddLevel(Coins);
                         LevelsView(level);
 
-                        if (level < 5){
-
-                        }else {
+                        if (level < 5) {
+                            ForButton3(level);
+                        } else {
                             Coins = c.Task1(Coins);
                             level = L.AddLevel(Coins);
                             LevelsView(level);
                             CoinsView(Coins);
                             ForButton2(level);
                             ForButton3(level);
+
+                            if (ClickB2 < 15){ ClickB2 ++;}
                         }
                     }
                 });
     }
-    void LevelsView(int level){
+
+    void LevelsView(int level) {
         TextView MyLevel = (TextView) findViewById(R.id.Level);
         String LevelString = Integer.toString(level);
         MyLevel.setText("Текущий уровень: " + LevelString);
     }
-    void CoinsView(int Coins){
+
+    void CoinsView(int Coins) {
         TextView MyCoins = (TextView) findViewById(R.id.Coins);
         String CoinsString = Integer.toString(Coins);
         MyCoins.setText(CoinsString);
     }
-    void ForButton1 (String Work){
+
+    void ForButton1(String Work) {
         Toast.makeText(
                 MainActivity.this, Work,
                 Toast.LENGTH_SHORT
         ).show();
-        }
 
-    void ForButton2(int level){
-        if (level > 1){
+    }
+
+    void ForButton2(int level) {
+        if (ClickB2 < 15 && level > 1 && !ClB2){
+            ForClickB2String = Integer.toString(15 - ClickB2);
+            if (15 - ClickB2 == 1)
+                btn2.setText("Будет доступно через 1 ход");
+            else if (15 - ClickB2 < 5 && 15 - ClickB2 > 1)
+                btn2.setText("Будет доступно через " + ForClickB2String + " хода");
+            else btn2.setText("Будет доступно через " + ForClickB2String + " ходов");
+            btn2.setBackgroundColor(Color.GRAY);
+            btn2.setClickable(false);
+            }else if (level > 1) {
             btn2.setText("Лотерея");
             btn2.setBackgroundColor(Color.YELLOW);
+            btn2.setClickable(true);
+            if (ClickB2 == 15 && ClB2){
+                ClickB2 = 0;
+                btn2.setText("Будет доступно через 15 ходов");
+                btn2.setBackgroundColor(Color.GRAY);
+                btn2.setClickable(false);
+            }
         }
     }
 
-    void ForButton3 (int level){
-        if (level > 4){
+    void ForButton3(int level) {
+        if (ClickB3 < 30 && level > 4 && !ClB3){
+            ForClickB3String = Integer.toString(30 - ClickB3);
+            if (30 - ClickB3 == 1)
+                btn3.setText("Будет доступно через 1 ход");
+            else if (30 - ClickB3 < 5 && 30 - ClickB3 > 1)
+                btn3.setText("Будет доступно через " + ForClickB3String + " хода");
+            else btn3.setText("Будет доступно через " + ForClickB3String + " ходов");
+            btn3.setBackgroundColor(Color.GRAY);
+            btn3.setClickable(false);
+        }else if (level > 4) {
             btn3.setText("Всё или ничего");
             btn3.setBackgroundColor(Color.RED);
+            btn3.setClickable(true);
+            if (ClickB3 == 30 && ClB3){
+                ClickB3 = 0;
+                btn3.setText("Будет доступно через 30 ходов");
+                btn3.setBackgroundColor(Color.GRAY);
+                btn3.setClickable(false);
+            }
         }
     }
 }
